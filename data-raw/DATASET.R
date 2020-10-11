9e16e785563e141557ac68a047e91e5e87242b32
mnissen1 <mnissen1@stanford.edu>
2020-10-11 16:01:54 -0700

    Fixed internal data error

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,5 +4,4 @@ ltbd_weights_00_10 <- readr::read_csv("data-raw/crosswalk_2000_2010.csv")
 
 ltbd_weights_90_10 <- readr::read_csv("data-raw/crosswalk_1990_2010.csv")
 
-usethis::use_data(ltbd_weights_00_10, overwrite = TRUE, internal = TRUE)
-usethis::use_data(ltbd_weights_90_10, overwrite = TRUE, internal = TRUE)
+usethis::use_data(ltbd_weights_00_10, ltbd_weights_90_10, overwrite = TRUE, internal = TRUE)
