ce737afec7528015e343bf74da59aa32da81c9ef
mnissen1 <mnissen1@stanford.edu>
2020-10-12 12:53:24 -0700

    Include variable specification argument

--- R/xwalk_00_10.R
+++ R/xwalk_00_10.R
@@ -3,21 +3,40 @@
 #' This function takes ACS/Census data with 2000 tract boundaries, and loads in
 #' weights from the LTBD
 #' (https://s4.ad.brown.edu/projects/diversity/Researcher/LTBDDload/DataList.aspx).
-#' It assumes that all numeric values in the specified ACS/Census data are to be
-#' crosswalked. If this is not the case, those variables should be filtered
-#' prior to using the function.
+#' It assumes that you want all variables to be crosswalked, and includes a filter
+#' to only choose numeric type variables. If you want to only crosswalk specific
+#' variables, specify this in the vars argument (must be numeric).
 #' It outputs the original data crosswalked to 2010 boundaries.
 #'
-#' @param census_data An R object containing ACS/Census data w/ 2000 boundaries
-#' @param tractID variable name that contains the tract ID
-#' @return Crosswalked ACS/Census data (2010 boundaries)
+#' @param census_data An R table containing ACS/Census data w/ 2000 boundaries
+#' @param tractID Variable name that contains the tract ID
+#' @param vars Census/ACS variables to crosswalk. Defaults to all numeric
+#' variables, but can be adjusted. Custom variables must be a single string
+#' or a list of strings.
+#' @return Crosswalked selected ACS/Census data (2010 boundaries, defaults to
+#' all numeric data)
 #' @export
-xwalk_00_10 <- function(census_data, tractID){
+xwalk_00_10 <- function(census_data, tractID, vars = names(census_data)){
   library(tidyverse)
   tractID <- enquo(tractID)
 
   # change tract ID name
-  census_data <- census_data %>% rename(trtid00 = !!quo_name(tractID))
+  census_data <-
+    census_data %>%
+    rename(trtid00 = !!quo_name(tractID)) %>%
+    mutate(
+      trtid00 = str_pad(as.character(trtid00), 11, side = "left", pad = "0")
+    )
+
+  # filter tract ID from variables
+  if (setequal(vars, names(census_data))) {
+    vars <-
+      census_data %>%
+      select(-trtid00) %>%
+      # make sure the data is type numeric
+      select_if(is.numeric) %>%
+      names()
+  }
 
   # FIPS filter from Census/ACS data
   fips_filter <-
@@ -38,8 +57,8 @@ xwalk_00_10 <- function(census_data, tractID){
     # multiply all data by its weight
     mutate(
       across(
-        # select all Census/ACS data
-        where(is.numeric),
+        # select selected Census/ACS data (defaults to everything)
+        .cols = vars,
         # find value of data multiplied by weight
         ~ . * weight,
         # attach name in the following pattern
@@ -54,8 +73,6 @@ xwalk_00_10 <- function(census_data, tractID){
     summarise_all(sum) %>%
     # ungroup
     ungroup() %>%
-    # select out unwanted weight vars
-    select(-c(weight_00:changetype_00)) %>%
     # remove the "_00" suffix
     rename_with(~str_remove(.x, "_00$"), ends_with("_00"))
 
